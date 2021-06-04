f28651d67ec1798d15fd3dfa83d7fda10161a877
Alok Goldy <alokkrgoldy@gmail.com>
2021-06-04 14:14:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(cors());
+
+
+app.get("/result/:roll",async (req,res)=>{
+
+    const arr = req.params.roll.split(',');
+    var result = [];
+
+    await Promise.all(
+        arr.map(async (data)=>{
+            var response = await fetch("http://proedge.me/test.php?rollnumber="+data,{
+                method:"GET",
+                headers: {"Content-type":"application/json"}
+            })
+            response =  await response.text();
+            result.push({
+                rollNo: Number(data),
+                result: response
+            });
+        })
+    );
+    
+    res.send(result);
+    result = [];
+
+})
+
+
+app.listen(port,()=>{
+    console.log(`backend is running on http://localhost:${port}`);
+})
